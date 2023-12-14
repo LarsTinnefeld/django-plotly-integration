317544a28a271b3b1e38c3b3d9d7be7bf3f8d624
Lars <l.tinnefeld@web.de>
2023-12-13 19:08:14 -0500

    Finished code
    Done

---
+++ core/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class DateForm(forms.Form):
+    start = forms.DateField(widget=forms.DateInput(attrs={
+        'type': 'date',
+    }))
+    end = forms.DateField(widget=forms.DateInput(attrs={
+        'type': 'date',
+    }))
--- core/views.py
+++ core/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+import plotly.express as px
+from core.models import CO2
+from core.forms import DateForm
+
 
 # Create your views here.
+
+
+def chart(request):
+    co2 = CO2.objects.all()
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+
+    if start:
+        co2 = co2.filter(date__gte=start)
+    if end:
+        co2 = co2.filter(date__lte=end)
+        # Filter "...__gte" is greater or equal to
+        # Filter "...__lte" is less or equal to
+
+    fig = px.line(
+        x=[c.date for c in co2],
+        y=[c.average for c in co2],
+        title='CO2 PPM',
+        labels={
+            'x': 'Date',
+            'y': 'CO2 PPM',
+        }
+    )
+
+    fig.update_layout(title={
+        'font_size': 22,
+        'xanchor': 'center',
+        'x': 0.5,
+    })
+
+    chart = fig.to_html()
+
+    context = {
+        'chart': chart,
+        'form': DateForm(),
+    }
+
+    return render(request, 'core/chart.html', context)
